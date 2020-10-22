967a81c564971a13fd0742821c4360924506d59b
Chris Dance <chrisjamesdance@gmail.com>
2020-10-22 21:14:42 +0200

    Adds Spinner size calculations

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,7 +1,30 @@
 import styles from "./Spinner.module.css";
 
-const Spinner = () => {
-  return <div className={styles.spinner}>Spinning away</div>;
+const Spinner = ({ percentage = 75, radius = 200, thickness = 20 }) => {
+  const circumference = 2 * Math.PI * radius;
+  const offset = circumference - circumference / (100 / percentage);
+  const padding = thickness / 2;
+
+  return (
+    <svg
+      className={styles.spinner}
+      height={(radius + padding) * 2}
+      viewBox={`0 0 ${(radius + padding) * 2} ${(radius + padding) * 2}`}
+      width={(radius + padding) * 2}
+    >
+      <circle
+        className={styles.progress}
+        r={radius}
+        cx={radius + padding}
+        cy={radius + padding}
+        style={{
+          strokeDasharray: circumference,
+          strokeDashoffset: offset,
+          strokeWidth: thickness,
+        }}
+      ></circle>
+    </svg>
+  );
 };
 
 export default Spinner;
