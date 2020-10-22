f114e214d6391ffa1bfbf9bdfd7f441216d834f4
Chris Dance <chrisjamesdance@gmail.com>
2020-10-22 21:35:26 +0200

    Adds percentage value as text

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,41 +1,47 @@
 import styles from "./Spinner.module.css";
 
-const Spinner = ({ percentage = 75, radius = 200, thickness = 20 }) => {
+const Spinner = ({ percentage = 45, radius = 200, thickness = 20 }) => {
   const circumference = 2 * Math.PI * radius;
   const offset = circumference - circumference / (100 / percentage);
   const padding = thickness / 2;
 
   return (
-    <svg
-      className={styles.spinner}
-      height={(radius + padding) * 2}
-      viewBox={`0 0 ${(radius + padding) * 2} ${(radius + padding) * 2}`}
-      width={(radius + padding) * 2}
-    >
-      <circle
-        className={styles.track}
-        r={radius + padding}
-        cx={radius + padding}
-        cy={radius + padding}
-      />
-      <circle
-        className={styles.progress}
-        r={radius}
-        cx={radius + padding}
-        cy={radius + padding}
-        style={{
-          strokeDasharray: circumference,
-          strokeDashoffset: offset,
-          strokeWidth: thickness,
-        }}
-      />
-      <circle
-        className={styles.inset}
-        r={radius - padding}
-        cx={radius + padding}
-        cy={radius + padding}
-      />
-    </svg>
+    <div className={styles.container}>
+      <svg
+        className={styles.spinner}
+        height={(radius + padding) * 2}
+        viewBox={`0 0 ${(radius + padding) * 2} ${(radius + padding) * 2}`}
+        width={(radius + padding) * 2}
+      >
+        <circle
+          className={styles.track}
+          r={radius + padding}
+          cx={radius + padding}
+          cy={radius + padding}
+        />
+        <circle
+          className={styles.progress}
+          r={radius}
+          cx={radius + padding}
+          cy={radius + padding}
+          style={{
+            strokeDasharray: circumference,
+            strokeDashoffset: offset,
+            strokeWidth: thickness,
+          }}
+        />
+        <circle
+          className={styles.inset}
+          r={radius - padding}
+          cx={radius + padding}
+          cy={radius + padding}
+        />
+      </svg>
+      <p className={styles.value}>
+        {percentage}
+        <span className={styles.percentage}>%</span>
+      </p>
+    </div>
   );
 };
 
