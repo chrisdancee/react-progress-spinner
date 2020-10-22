7619fcffd3394a57f48e24aea7a513f2b0e62392
Chris Dance <chrisjamesdance@gmail.com>
2020-10-22 23:35:49 +0200

    Vercel deploy fix

--- pages/components/index.js
+++ pages/components/index.js
@@ -2,3 +2,7 @@ export { default as Button } from "./Button";
 export { default as Card } from "./Card";
 export { default as Container } from "./Container";
 export { default as Content } from "./Content";
+
+// This is just to appease Vercel Now deploy :( https://github.com/vercel/next.js/issues/7836
+const Named = () => <div />;
+export default Named;
